fix KL

KGReasoning/main.py
@@ -26,19 +26,19 @@ from TimeCounter import TimeCounter
 
 
 # Print Fx Graph in Torch Dynamo
-from torch._dynamo import optimize
-from torch._inductor import graph
-class CustomGraphLowering(graph.GraphLowering):
-    def __init__(
-        self,
-        gm: torch.fx.GraphModule,
-        *args,
-        **kwargs,
-    ):
-        super().__init__(gm, *args, **kwargs)
-        print("*"*30 + "Print Fx Graph" + "*"*30)
-        # gm.graph.print_tabular()
-graph.GraphLowering = CustomGraphLowering
+# from torch._dynamo import optimize
+# from torch._inductor import graph
+# class CustomGraphLowering(graph.GraphLowering):
+#     def __init__(
+#         self,
+#         gm: torch.fx.GraphModule,
+#         *args,
+#         **kwargs,
+#     ):
+#         super().__init__(gm, *args, **kwargs)
+#         print("*"*30 + "Print Fx Graph" + "*"*30)
+#         gm.graph.print_tabular()
+# graph.GraphLowering = CustomGraphLowering
 
 
 # Regist Operator in Hidet
@@ -393,12 +393,12 @@ def main(args):
         test_batch_size=args.test_batch_size,
         query_name_dict = query_name_dict
     )
-    model = torch.compile(model, backend="inductor")   
+    # model = torch.compile(model, backend="inductor")
 
     # hidet.torch.dynamo_config.print_input_graph(True)
     # hidet.torch.dynamo_config.dump_graph_ir("graph_hidet")
     # hidet.torch.dynamo_config.correctness_report()
-    # model = torch.compile(model, backend="hidet")
+    model = torch.compile(model, backend="hidet")
 
     logging.info('Model Parameter Configuration:')
     num_params = 0
@@ -521,17 +521,18 @@ def main(args):
             # test_all_metrics = evaluate(model, test_easy_answers, test_hard_answers, args, test_dataloader, query_name_dict, 'Test', step, writer)
         # print(prof.key_averages().table(sort_by="self_cpu_time_total"))
         
-        from torch.profiler import profile, record_function, ProfilerActivity
+        # from torch.profiler import profile, record_function, ProfilerActivity
         # with torch.autograd.profiler.profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
-        with torch.autograd.profiler.profile(enabled=True) as prof:
-            test_all_metrics = evaluate(model, test_easy_answers, test_hard_answers, args, test_dataloader, query_name_dict, 'Test', step, writer)    
-        print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+        # with torch.autograd.profiler.profile(enabled=True) as prof:
+        test_all_metrics = evaluate(model, test_easy_answers, test_hard_answers, args, test_dataloader, query_name_dict, 'Test', step, writer)    
+        # print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
         
         # with open("output.txt", 'a') as f:
         #     print("hidet", test_all_metrics, file=f)
         #     print("*"*99, file=f)
         
     logging.info("Training finished!!")
+
 
 if __name__ == '__main__':
     main(parse_args())

KGReasoning/models.py
@@ -312,7 +312,7 @@ class KGReasoning(nn.Module):
     def cal_logit_beta(self, entity_embedding, query_dist):
         alpha_embedding, beta_embedding = torch.chunk(entity_embedding, 2, dim=-1)
         entity_dist = torch.distributions.beta.Beta(alpha_embedding, beta_embedding)
-        logit = self.gamma - torch.norm(torch.distributions.kl.kl_divergence(entity_dist, query_dist), p=1, dim=-1)
+        logit = self.gamma - torch.norm(torch.distributions.kl._kl_beta_beta(entity_dist, query_dist), p=1, dim=-1)
         return logit
 
     def forward_beta(self, positive_sample, negative_sample, subsampling_weight, batch_queries_dict, batch_idxs_dict):

KGReasoning/test.py
@@ -61,6 +61,18 @@ def test_norm():
     print("hidet: ", y_hidet.shape)
     print(y_hidet)
 
-test_norm()
 
+def test_broadcast_tensor():
+    x = torch.arange(3).view(1, 3)
+    y = torch.arange(2).view(2, 1)
+    z = torch.arange(4).view(2, 2)
+    a, b, c = torch.broadcast_tensors(x, y, z)
+    print(a, b, c)
+
+
+def main():
+    test_broadcast_tensor()
+
+if __name__ == "__main__":
+    main()
 